7cecd98f23d12aec20e42b54d3b7507379af7141
Craig <chillsdon@gmail.com>
2021-11-03 23:53:55 +0000

    Playing with animations and buttons

--- code.py
+++ code.py
@@ -5,41 +5,35 @@ import time
 import board
 import neopixel
 import microcontroller
-#import digitalio
-#from adafruit_debouncer import Debouncer
-#from adafruit_led_animation.sequence import AnimationSequence
-#from adafruit_led_animation.animation.blink import Blink
-from adafruit_led_animation.animation.comet import Comet
-#from adafruit_led_animation.animation.chase import Chase
-#from adafruit_led_animation.animation.sparkle import Sparkle
+import digitalio
+from adafruit_debouncer import Debouncer
+from adafruit_led_animation.sequence import AnimationSequence
 
-#from adafruit_led_animation.color import RED
-#from adafruit_led_animation.color import YELLOW
-from adafruit_led_animation.color import ORANGE
-#from adafruit_led_animation.color import GREEN
-from adafruit_led_animation.color import TEAL
-#from adafruit_led_animation.color import CYAN
-#from adafruit_led_animation.color import BLUE
-#from adafruit_led_animation.color import PURPLE
-from adafruit_led_animation.color import MAGENTA
-#from adafruit_led_animation.color import WHITE
-#from adafruit_led_animation.color import BLACK
-#from adafruit_led_animation.color import GOLD
-#from adafruit_led_animation.color import PINK
-#from adafruit_led_animation.color import AQUA
-from adafruit_led_animation.color import JADE
-#from adafruit_led_animation.color import AMBER
-#from adafruit_led_animation.color import OLD_LACE
+from adafruit_led_animation.animation.colorcycle import ColorCycle
+from adafruit_led_animation.animation.blink import Blink
+from adafruit_led_animation.animation.comet import Comet
+from adafruit_led_animation.animation.pulse import Pulse
+from adafruit_led_animation.animation.chase import Chase
+
+from adafruit_led_animation.animation.sparkle import Sparkle
+from adafruit_led_animation.animation.SparklePulse import SparklePulse
+
+from adafruit_led_animation.animation.rainbow import Rainbow
+from adafruit_led_animation.animation.rainbowchase import RainbowChase
+from adafruit_led_animation.animation.rainbowcomet import RainbowComet
+from adafruit_led_animation.animation.rainbowsparkle import RainbowSparkle
+
+from adafruit_led_animation.color import RED, YELLOW, YELLOW, ORANGE, GREEN, TEAL, CYAN, BLUE, PURPLE, MAGENTA, WHITE, BLACK, GOLD, PINK, AQUA, JADE, AMBER, OLD_LACE
 
 # On CircuitPlayground Express, and boards with built in status NeoPixel -> board.NEOPIXEL
 # Otherwise choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D1
-pixel_pin = board.D0
+pixel_pin = board.D12
 
 # The number of NeoPixels
-num_pixels = 68
+num_pixels = 72
 
-#mode = 1
-#maxMode = 4
+mode = 1
+maxMode = 4
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
@@ -49,57 +43,62 @@ pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.1, auto_write=False, pixel_order=ORDER
 )
 
-pixelColour = TEAL
+#pixelColour = AMBER
 
-#colourInt = random.randint(1, 4)
+pin = digitalio.DigitalInOut(board.D10)
+pin.direction = digitalio.Direction.INPUT
+pin.pull = digitalio.Pull.UP
+switch = Debouncer(pin)
 
-#if colourInt == 1:
-#    pixelColour = ORANGE
+#Basic Animations
+colorcycle = ColorCycle(pixels, 0.5, colors=[MAGENTA, ORANGE, TEAL])
+blink = Blink(pixels, speed=0.5, color=ORANGE)
+comet = Comet(pixels, speed=0.02, color=MAGENTA, tail_length=10, bounce=True)
+pulse = Pulse(pixels, speed=0.1, color=AMBER, period=3)
+chase = Chase(pixels, speed=0.1, size=3, spacing=6, color=AMBER)
 
-#if colourInt == 2:
-#    pixelColour = TEAL
+#Sparkle Animations
+sparkle = Sparkle(pixels, speed=0.05, color=TEAL, num_sparkles=10)
+sparkle_pulse = SparklePulse(pixels, speed=0.05, period=3, color=JADE)
 
-#if colourInt == 3:
-#    pixelColour = JADE
-
-#if colourInt == 4:
-#    pixelColour = MAGENTA
-
-#pin = digitalio.DigitalInOut(board.D3)
-#pin.direction = digitalio.Direction.INPUT
-#pin.pull = digitalio.Pull.UP
-#switch = Debouncer(pin)
-
-#blink = Blink(pixels, speed=0.5, color=ORANGE)
-comet = Comet(pixels, speed=0.01, color=pixelColour, tail_length=10, bounce=True)
-#chase = Chase(pixels, speed=0.1, size=3, spacing=6, color=AMBER)
-#sparkle = Sparkle(pixels, speed=0.05, color=pixelColour, num_sparkles=10)
+#Rainbow Animations
+rainbow = Rainbow(pixels, speed=0.1, period=2)
+rainbow_chase = RainbowChase(pixels, speed=0.1, size=5, spacing=3)
+rainbow_comet = RainbowComet(pixels, speed=0.1, tail_length=7, bounce=True)
+rainbow_sparkle = RainbowSparkle(pixels, speed=0.1, num_sparkles=15)
 
 #animations = AnimationSequence(
-#    blink, comet, advance_interval=5, auto_clear=True, random_order=True
+#    blink, comet, chase, sparkle, advance_interval=5, auto_clear=True
 #)
 
+animations = AnimationSequence(
+    colorcycle, blink, comet, pulse, chase, sparkle, sparkle_pulse, rainbow, rainbow_chase, rainbow_comet, rainbow_sparkle, auto_clear=True
+)
+
+display = chase
+
 while True:
-    #switch.update()
+    #display.animate()
+    animations.animate()
 
-    #if switch.fell:
-    #    microcontroller.reset()
+    switch.update()
 
-    #    mode = mode + 1
+    if switch.fell:
+        animations.next()        
 
-    #    if mode > maxMode:
-    #        mode = 1
+        #mode += 1
 
-    #    if mode == 1:
-    #        comet = Comet(pixels, speed=0.01, color=ORANGE, tail_length=10, bounce=True)
+        #if mode > maxMode:
+        #    mode = 1
 
-    #    if mode == 1:
-    #        comet = Comet(pixels, speed=0.01, color=MAGENTA, tail_length=10, bounce=True)
+        #if mode == 1:
+        #    display.color = AMBER
 
-    #    if mode == 1:
-    #        comet = Comet(pixels, speed=0.01, color=AQUA, tail_length=10, bounce=True)
+        #if mode == 2:
+        #    display.color = MAGENTA
 
-    #    if mode == 1:
-    #        comet = Comet(pixels, speed=0.01, color=JADE, tail_length=10, bounce=True)
+        #if mode == 3:
+        #    display.color = TEAL
 
-    comet.animate()
+        #if mode == 4:
+        #    display.color = JADE
