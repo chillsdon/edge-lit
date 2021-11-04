e8c92e93676a020a22e3c63cb499f13fbaaf9bf3
Craig <chillsdon@gmail.com>
2021-11-04 22:21:23 +0000

    Added a second button and solid colour.
    First button toggles animation. Second button toggles the colour for the single colour (non-rainbow) animations.

--- code.py
+++ code.py
@@ -9,7 +9,8 @@ import digitalio
 from adafruit_debouncer import Debouncer
 from adafruit_led_animation.sequence import AnimationSequence
 
-from adafruit_led_animation.animation.colorcycle import ColorCycle
+from adafruit_led_animation.animation.solid import Solid
+#from adafruit_led_animation.animation.colorcycle import ColorCycle
 from adafruit_led_animation.animation.blink import Blink
 from adafruit_led_animation.animation.comet import Comet
 from adafruit_led_animation.animation.pulse import Pulse
@@ -23,7 +24,7 @@ from adafruit_led_animation.animation.rainbowchase import RainbowChase
 from adafruit_led_animation.animation.rainbowcomet import RainbowComet
 from adafruit_led_animation.animation.rainbowsparkle import RainbowSparkle
 
-from adafruit_led_animation.color import RED, YELLOW, YELLOW, ORANGE, GREEN, TEAL, CYAN, BLUE, PURPLE, MAGENTA, WHITE, BLACK, GOLD, PINK, AQUA, JADE, AMBER, OLD_LACE
+from adafruit_led_animation.color import RED, YELLOW, ORANGE, GREEN, TEAL, CYAN, BLUE, PURPLE, MAGENTA, WHITE, BLACK, GOLD, PINK, AQUA, JADE, AMBER, OLD_LACE
 
 # On CircuitPlayground Express, and boards with built in status NeoPixel -> board.NEOPIXEL
 # Otherwise choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D1
@@ -43,28 +44,37 @@ pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.1, auto_write=False, pixel_order=ORDER
 )
 
-#pixelColour = AMBER
+colourArray = [RED, YELLOW, ORANGE, GREEN, TEAL, CYAN, BLUE, PURPLE, MAGENTA, WHITE, GOLD, PINK, AQUA, JADE, AMBER, OLD_LACE]
+colourArrayIndex = 0
 
-pin = digitalio.DigitalInOut(board.D10)
-pin.direction = digitalio.Direction.INPUT
-pin.pull = digitalio.Pull.UP
-switch = Debouncer(pin)
+pixelColour = colourArray[colourArrayIndex]
+
+pin1 = digitalio.DigitalInOut(board.D10)
+pin1.direction = digitalio.Direction.INPUT
+pin1.pull = digitalio.Pull.UP
+switch1 = Debouncer(pin1)
+
+pin2 = digitalio.DigitalInOut(board.D8)
+pin2.direction = digitalio.Direction.INPUT
+pin2.pull = digitalio.Pull.UP
+switch2 = Debouncer(pin2)
 
 #Basic Animations
-colorcycle = ColorCycle(pixels, 0.5, colors=[MAGENTA, ORANGE, TEAL])
-blink = Blink(pixels, speed=0.5, color=ORANGE)
-comet = Comet(pixels, speed=0.02, color=MAGENTA, tail_length=10, bounce=True)
-pulse = Pulse(pixels, speed=0.1, color=AMBER, period=3)
-chase = Chase(pixels, speed=0.1, size=3, spacing=6, color=AMBER)
+solid = Solid(pixels, color=pixelColour)
+#colorcycle = ColorCycle(pixels, 0.5, colors=[MAGENTA, ORANGE, TEAL])
+blink = Blink(pixels, speed=0.5, color=pixelColour)
+comet = Comet(pixels, speed=0.02, color=pixelColour, tail_length=10, bounce=True)
+pulse = Pulse(pixels, speed=0.1, color=pixelColour, period=3)
+chase = Chase(pixels, speed=0.1, size=3, spacing=6, color=pixelColour)
 
 #Sparkle Animations
-sparkle = Sparkle(pixels, speed=0.05, color=TEAL, num_sparkles=10)
-sparkle_pulse = SparklePulse(pixels, speed=0.05, period=3, color=JADE)
+sparkle = Sparkle(pixels, speed=0.05, color=pixelColour, num_sparkles=10)
+sparkle_pulse = SparklePulse(pixels, speed=0.05, period=3, color=pixelColour)
 
 #Rainbow Animations
 rainbow = Rainbow(pixels, speed=0.1, period=2)
 rainbow_chase = RainbowChase(pixels, speed=0.1, size=5, spacing=3)
-rainbow_comet = RainbowComet(pixels, speed=0.1, tail_length=7, bounce=True)
+rainbow_comet = RainbowComet(pixels, speed=0.02, tail_length=7, bounce=True)
 rainbow_sparkle = RainbowSparkle(pixels, speed=0.1, num_sparkles=15)
 
 #animations = AnimationSequence(
@@ -72,33 +82,25 @@ rainbow_sparkle = RainbowSparkle(pixels, speed=0.1, num_sparkles=15)
 #)
 
 animations = AnimationSequence(
-    colorcycle, blink, comet, pulse, chase, sparkle, sparkle_pulse, rainbow, rainbow_chase, rainbow_comet, rainbow_sparkle, auto_clear=True
+    solid, blink, comet, pulse, chase, sparkle, sparkle_pulse, rainbow, rainbow_chase, rainbow_comet, rainbow_sparkle, auto_clear=True
 )
 
-display = chase
-
 while True:
-    #display.animate()
     animations.animate()
 
-    switch.update()
+    switch1.update()
 
-    if switch.fell:
+    if switch1.fell:
         animations.next()        
 
-        #mode += 1
+    switch2.update()
 
-        #if mode > maxMode:
-        #    mode = 1
+    if switch2.fell:
+        colourArrayIndex += 1
 
-        #if mode == 1:
-        #    display.color = AMBER
+        if colourArrayIndex > len(colourArray) -1:
+            colourArrayIndex = 0
 
-        #if mode == 2:
-        #    display.color = MAGENTA
+        pixelColour = colourArray[colourArrayIndex]
 
-        #if mode == 3:
-        #    display.color = TEAL
-
-        #if mode == 4:
-        #    display.color = JADE
+        animations.color = pixelColour
