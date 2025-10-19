1436efb1b34b2e35bc6cb4e2f795407c5813a861
Craig <chillsdon@gmail.com>
2025-10-19 11:43:05 +0100

    Updated to CircuitPython 10, added laser cut profiles and parts list.

--- README.md
+++ README.md
@@ -1,9 +1,48 @@
 edge-lit
 
+
+
 CircuitPython code to run on an M4 chip (ItsyBitsy M4 Express) to run the Adafruit LED Animations library with two buttons; one to advance to the next animation, one to advance the colour of the single colour based animations.
 
-Final design will sit in a 3D printed enclosure with a laser cut lid that will incorporate a slot that lines up with the strip of NeoPixels. A transparent Perspex "sign" that has been laser engraved can then be inserted into the slot and be edge lit with any of the animations/colours.
+
+
+Final design will sit in a laser cut enclosure that will incorporate a slot that lines up with the strip of NeoPixels. A transparent Perspex "sign" that has been laser engraved can then be inserted into the slot and be edge lit with any of the animations/colours.
+
+
 
 Multiple signs can be created and swapped out as required.
 
-Code is CircuitPython so that the unit can be connected to a computer and the code easily changed (hopefully engaging, educational and fun). 
+
+
+Code is CircuitPython so that the unit can be connected to a computer and the code easily changed (hopefully engaging, educational and fun).
+
+
+
+Now upgraded to CircuitPython 10.
+
+
+
+DXF drawings for laser cutting in the Profiles folder (all 5mm thick and all are 240mm x 67mm).
+
+
+
+Images to be engraved on 5mm thick transparent Perspex with a 3mm radius on all 4 corners. These "signs" should be 200mm wide but the height will depend on the aspect ratio of the image. General rule: image scaled to be 180mm wide and engraved 10mm down from the top edge and 10mm in from either side. The distance from the bottom edge of the image to the bottom edge of the Perspex to be 53mm.
+
+
+
+https://youtu.be/CMc5AJtdoJ4
+
+
+
+Parts
+
+ItsyBitsy M4 Express
+
+NeoPixels
+
+https://www.amazon.co.uk/dp/B07QB68F9J
+
+https://www.amazon.co.uk/dp/B0CG1WSC99
+
+https://www.amazon.co.uk/dp/B07N1N1T7R
+
--- code.py
+++ code.py
@@ -1,5 +1,4 @@
 #https://learn.adafruit.com/circuitpython-led-animations/overview
-
 import time
 import board
 import neopixel
@@ -7,29 +6,23 @@ import microcontroller
 import digitalio
 from adafruit_debouncer import Debouncer
 from adafruit_led_animation.sequence import AnimationSequence
-
 from adafruit_led_animation.animation.solid import Solid
 #from adafruit_led_animation.animation.colorcycle import ColorCycle
 from adafruit_led_animation.animation.blink import Blink
 from adafruit_led_animation.animation.comet import Comet
 from adafruit_led_animation.animation.pulse import Pulse
 from adafruit_led_animation.animation.chase import Chase
-
 from adafruit_led_animation.animation.sparkle import Sparkle
-from adafruit_led_animation.animation.SparklePulse import SparklePulse
-
+from adafruit_led_animation.animation.sparklepulse import SparklePulse
 from adafruit_led_animation.animation.rainbow import Rainbow
 from adafruit_led_animation.animation.rainbowchase import RainbowChase
 from adafruit_led_animation.animation.rainbowcomet import RainbowComet
 from adafruit_led_animation.animation.rainbowsparkle import RainbowSparkle
-
 from adafruit_led_animation.color import RED, YELLOW, ORANGE, GREEN, TEAL, CYAN, BLUE, PURPLE, MAGENTA, WHITE, BLACK, GOLD, PINK, AQUA, JADE, AMBER, OLD_LACE
 
 pixel_pin = board.D12
-
 # The number of NeoPixels
-num_pixels = 72
-
+num_pixels = 29
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
 ORDER = neopixel.GRB
@@ -40,7 +33,6 @@ pixels = neopixel.NeoPixel(
 
 colourArray = [RED, YELLOW, ORANGE, GREEN, TEAL, CYAN, BLUE, PURPLE, MAGENTA, WHITE, GOLD, PINK, AQUA, JADE, AMBER, OLD_LACE]
 colourArrayIndex = 0
-
 pixelColour = colourArray[colourArrayIndex]
 
 pin1 = digitalio.DigitalInOut(board.D10)
@@ -48,12 +40,12 @@ pin1.direction = digitalio.Direction.INPUT
 pin1.pull = digitalio.Pull.UP
 switch1 = Debouncer(pin1)
 
-pin2 = digitalio.DigitalInOut(board.D8)
+pin2 = digitalio.DigitalInOut(board.D7)
 pin2.direction = digitalio.Direction.INPUT
 pin2.pull = digitalio.Pull.UP
 switch2 = Debouncer(pin2)
 
-#Basic Animations
+# Basic Animations
 solid = Solid(pixels, color=pixelColour)
 #colorcycle = ColorCycle(pixels, 0.5, colors=[MAGENTA, ORANGE, TEAL])
 blink = Blink(pixels, speed=0.5, color=pixelColour)
@@ -61,36 +53,44 @@ comet = Comet(pixels, speed=0.02, color=pixelColour, tail_length=10, bounce=True
 pulse = Pulse(pixels, speed=0.1, color=pixelColour, period=3)
 chase = Chase(pixels, speed=0.1, size=3, spacing=6, color=pixelColour)
 
-#Sparkle Animations
+# Sparkle Animations
 sparkle = Sparkle(pixels, speed=0.05, color=pixelColour, num_sparkles=10)
 sparkle_pulse = SparklePulse(pixels, speed=0.05, period=3, color=pixelColour)
 
-#Rainbow Animations
+# Rainbow Animations
 rainbow = Rainbow(pixels, speed=0.1, period=2)
 rainbow_chase = RainbowChase(pixels, speed=0.1, size=5, spacing=3)
 rainbow_comet = RainbowComet(pixels, speed=0.02, tail_length=7, bounce=True)
 rainbow_sparkle = RainbowSparkle(pixels, speed=0.1, num_sparkles=15)
 
+# Full animation sequence - disable auto-advance so we can control it manually
 animations = AnimationSequence(
-    solid, blink, comet, pulse, chase, sparkle, sparkle_pulse, rainbow, rainbow_chase, rainbow_comet, rainbow_sparkle, auto_clear=True
+    solid, blink, comet, pulse, chase, sparkle, sparkle_pulse,
+    rainbow, rainbow_chase, rainbow_comet, rainbow_sparkle,
+    auto_clear=True,
+    advance_interval=None  # Disable auto-advance
 )
 
 while True:
     animations.animate()
 
     switch1.update()
-
     if switch1.fell:
-        animations.next()        
+        print("Button 1 pressed - switching animation")  # Debug output
+        animations.next()
 
     switch2.update()
-
     if switch2.fell:
+        print("Button 2 pressed - changing colour")  # Debug output
         colourArrayIndex += 1
-
-        if colourArrayIndex > len(colourArray) -1:
+        if colourArrayIndex > len(colourArray) - 1:
             colourArrayIndex = 0
-
         pixelColour = colourArray[colourArrayIndex]
-
-        animations.color = pixelColour
+        # Update each animation's colour (rainbow animations don't support colour changes)
+        solid.color = pixelColour
+        blink.color = pixelColour
+        comet.color = pixelColour
+        pulse.color = pixelColour
+        chase.color = pixelColour
+        sparkle.color = pixelColour
+        sparkle_pulse.color = pixelColour
